f094c9fd0aafde5e5015ed7df2c57a8bdc80c5fd
ada-ip <ada.iglesias.pasamontes@gmail.com>
2023-10-03 16:55:28 +0200

    Add Navbar and NavbarLink components with styles

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,39 @@
+<script setup>
+// Components
+import NavbarLink from "./NavbarLink.vue";
+
+// Outside data
+import sublinks from "../data/data";
+</script>
+
 <template>
-	<nav class="navbar">Strapi</nav>
+	<nav class="navbar">
+		<span class="navbar__logo">strapi</span>
+		<ul class="navbar__links-list">
+			<NavbarLink v-for="{ pageId, page } in sublinks" :key="pageId" :url="page" :text="page" />
+		</ul>
+	</nav>
 </template>
 
 <style scoped>
 .navbar {
-	position: fixed;
+	padding-block: 1.4rem;
+	position: absolute;
 	inset: 0 0 auto 0;
+	display: flex;
+	align-items: center;
+}
+
+.navbar__logo {
+	font-size: 1.8rem;
+	font-weight: 700;
+}
+
+.navbar__links-list {
+	flex-grow: 1;
+	display: flex;
+	justify-content: center;
+	list-style: none;
+	gap: 1.2rem;
 }
 </style>
---
+++ src/components/NavbarLink.vue
@@ -0,0 +1,41 @@
+<script setup>
+// Props
+defineProps({
+	url: {
+		type: String,
+		required: true
+	},
+	text: {
+		type: String,
+		required: true
+	}
+});
+</script>
+
+<template>
+	<li class="navbar-option">
+		<a :href="url" class="navbar-option__link">{{ text }} <i class="fa-solid fa-angle-down"></i></a>
+	</li>
+</template>
+
+<style scoped>
+.navbar-option__link {
+	padding: 0.4em 0.7em;
+	border-radius: 5px;
+	color: var(--white-color);
+	text-decoration: none;
+	text-transform: capitalize;
+}
+
+.navbar-option__link:hover,
+.navbar-option__link:focus {
+	background-color: var(--darker-bground);
+}
+
+.fa-angle-down {
+	font-size: 0.6rem;
+	position: relative;
+	top: -1px;
+	left: 2px;
+}
+</style>
